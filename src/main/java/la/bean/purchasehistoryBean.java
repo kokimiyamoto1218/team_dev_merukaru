840446e28ff292b2d51fd049bc890f69ffc90382
takumihatano808 <727-student027@la-study.com>
2026-05-21 11:37:38 +0900

    追加

---
+++ src/main/java/la/bean/purchasehistoryBean.java
@@ -0,0 +1,53 @@
+package la.bean;
+
+import java.io.Serializable;
+
+public class purchasehistoryBean implements Serializable {
+	private int code;
+	private int catecode;
+	private String name;
+	private int price;
+
+	public purchasehistoryBean(int code, int catecode,String name, int price) {
+		this.code = code;
+		this.catecode = catecode;
+		this.name = name;
+		this.price = price;
+	}
+
+	public purchasehistoryBean() {
+
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+	public int getCatecode() {
+		return catecode;
+	}
+
+	public void setCatecode(int catecode) {
+		this.catecode = catecode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
