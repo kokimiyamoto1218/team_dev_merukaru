356c5fe0e9b96915e132d398704f030d03a82471
konishi219 <727-student010@la-study.com>
2026-05-21 10:26:57 +0900

    konshi 訂正

--- src/main/java/la/servlet/ItemServlet.java
+++ src/main/java/la/servlet/ItemServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/SystemServlet")
+@WebServlet("/ItemServlet")
 public class ItemServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request,
